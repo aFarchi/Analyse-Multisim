2ad4aa9f0deb81a7e3b6034816c368c8cf84cba4
aFarchi <alban.farchi@free.fr>
2015-06-06 17:34:38 +0200

    added interpolation in gray scale

--- analyse/utils/analyse/scaling/grayScale.py
+++ analyse/utils/analyse/scaling/grayScale.py
@@ -8,28 +8,42 @@ import numpy as np
 
 def makeGrayScale(matrix, levels=None, mini=None, maxi=None, nLevels=32, threshold=None):
 
+    # make levels
     if levels is None:
-        levels = np.linspace(mini, maxi, nLevels)
+        levels  = np.linspace(mini, maxi, nLevels)
     else:
         nLevels = len(levels)
 
+    try:
+        spacing = levels[1] - levels[0]
+    except:
+        spacing = 1.0
+    if spacing == 0.0:
+        spacing = 1.0
+
+    # threshold
     if threshold is None:
         threshold = matrix.min() - 1
 
-    CDF = np.zeros(nLevels+1)
+    # compute CDF
+    CDF = np.zeros(nLevels)
+    for i in xrange(nLevels-2):
+        CDF[i+1]   = ( ( matrix < levels[i] ) * ( matrix > threshold ) ).mean()
+    CDF[nLevels-1] = ( matrix > threshold ).mean()
 
-    for i in xrange(nLevels-1):
-        CDF[i+1] = ( ( matrix < levels[i] ) * ( matrix > threshold ) ).mean()
-
-    CDF[nLevels] = ( matrix > threshold ).mean()
-
-    if CDF[nLevels] == 0:
-        CDF    = np.ones(nLevels+1)
+    # rescale CDF
+    if CDF[nLevels-1] == 0.0:
+        CDF    = np.ones(nLevels)
         CDF[0] = 0.0
     else:
-        CDF /= CDF[nLevels]
+        CDF   /= CDF[nLevels-1]
 
-    PDF = CDF[1:nLevels+1] - CDF[0:nLevels]
+    # derivate CDF
+    dCDF = ( CDF[1:] - CDF[:nLevels-1] ) / spacing
+
+    # interpolate PDF = derivate(CDF)
+    PDF  = np.zeros(nLevels)
+    PDF[1:nLevels-1] = ( dCDF[1:] + dCDF[:nLevels-2] ) / 2.0
     return PDF
 
 #__________________________________________________
