corrected fm scaling

analyse/utils/analyse/fields/field.py
@@ -2,11 +2,9 @@
 # field.py
 #_________
 
-from ..processRawData.interpolateRawData    import interpolateRawData
-from ..scaling.scaling                      import computeScaling
-from ..scaling.fmScaling                    import computeFMScaling
-from ..scaling.grayScale                    import makeGrayScale
-from ..timeSelection.defaultTSelect         import selectLastT
+from ..processRawData.interpolateRawData import interpolateRawData
+from ..scaling.scaling                   import computeScaling
+from ..timeSelection.defaultTSelect      import selectLastT
 
 #__________________________________________________
 
@@ -55,34 +53,6 @@ class Field:
             mini = 0.0
 
         return np.maximum(data, mini)
-
-    #_________________________
-
-    def computeGrayScale(self, data, mini, maxi, nLevels, threshold=True):
-        if threshold:
-            threshold = self.minValue
-        else:
-            threshold = None
-        return makeGrayScale(data, mini=mini, maxi=maxi, nLevels=nLevels, threshold=threshold)
-
-    #_________________________
-
-    def computeFMScaling(self, rawData, LOL, mini, maxi, nLevels):
-        t    = self.funTSelect(rawData.shape[0])
-        data = self.extraction(rawData[t], LOL, TS) 
-        return computeFMScaling(data, mini=mini, maxi=maxi, nLevels=nLevels)
-
-    #_________________________
-
-    #def computeFMScalingMakeGrayScale(self, rawData, LOL, TS, mini, maxi, nLevels):
-    #    t         = self.funTSelect(rawData.shape[0])
-    #    data      = self.extraction(rawData[t], lol, 0.5)
-
-    #    FMScaling = computeFMScaling(data, mini=mini, maxi=maxi, nLevels=nLevels)
-    #    GSNT      = self.computeGrayScale(data, mini, maxi, nLevels, False)
-    #    GST       = self.computeGrayScale(data, mini, maxi, nLevels, True)
-
-    #    return (FMScaling, GSNT, GST)
 
     #_________________________
 

analyse/utils/analyse/filters/zeroFilterLog10.py
@@ -6,18 +6,26 @@ import numpy as np
 
 #__________________________________________________
 
+def minValueFiltered():
+    return ( - 1.0 )
+
+#__________________________________________________
+
+def halfMinValueFiltered():
+    return ( - 0.5 )
+
+#__________________________________________________
+
 def zeroFilterLog10(rawData, minValue, LOL):
 
-    minValueFiltered = - 1.0
-
     if LOL == 'lin':
-        return ( minValueFiltered * ( rawData <= minValue ) +
-                 rawData          * ( rawData >  minValue ) )
+        return ( minValueFiltered() * ( rawData <= minValue ) +
+                 rawData            * ( rawData >  minValue ) )
     
     elif LOL == 'log':
         data = np.log10(np.maximum(rawData, 0.1 * minValue)) - np.log10(minValue)
 
-        return ( minValueFiltered * ( data <= 0.0 ) +
-                 data             * ( data >  0.0 ) )
+        return ( minValueFiltered() * ( data <= 0.0 ) +
+                 data               * ( data >  0.0 ) )
     
 #__________________________________________________