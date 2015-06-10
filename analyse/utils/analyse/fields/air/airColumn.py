corrected scaling of fields

analyse/utils/analyse/fields/air/airColumn.py
@@ -17,14 +17,14 @@ class AirColumn(Field):
 
     #_________________________
 
-    def extraction(self, rawData, LOL, TS):
-        data = zeroFilterLog10(rawData, self.minValue, LOL, TS)
+    def extraction(self, rawData, LOL):
+        data = zeroFilterLog10(rawData, self.minValue, LOL)
         return ( np.average(data[:,:,:], axis=0, weights=self.weights).transpose() )
 
     #_________________________
 
-    def extractionAllIterations(self, rawData, LOL, TS):
-        data = zeroFilterLog10(rawData,self.minValue, LOL, TS)
+    def extractionAllIterations(self, rawData, LOL):
+        data = zeroFilterLog10(rawData,self.minValue, LOL)
         return ( np.average(data[:,:,:,:], axis=1, weights=self.weights).transpose((0,2,1)) )
                                     
 #__________________________________________________

analyse/utils/analyse/fields/field.py
@@ -26,27 +26,35 @@ class Field:
 
     #_________________________
 
-    def extract(self, rawData, LOL, TS):
-        t     = self.funTSelect(rawData.shape[0])
-        data  = self.extraction(rawData[t], LOL, TS)
-        scale = computeScaling(data)
+    def extract(self, rawData, LOL, copy=True):
+        t               = self.funTSelect(rawData.shape[0])
+        data            = self.extraction(rawData[t], LOL)
 
-        if LOL == 'lin':
-            scale.mini = self.minValue
-            if scale.maxi <= self.minValue:
-                scale.maxi = 2 * self.minValue
+        defaultMaxValue = 2.0 * self.minValue
+        if LOL == 'log':
+            defaultMaxValue = np.log10(2.0)
 
-        elif LOL == 'log':
-            scale.mini = 0.0
-            if scale.maxi <= 0.0:
-                scale.maxi = 1.0
+        scale = computeScaling(data, self.minValue, defaultMaxValue)
 
-        return (data, scale)
+        if copy:
+            return (data.copy(), scale)
+        else:
+            return (data, scale)
 
     #_________________________
 
-    def extractAllIterations(self, rawData, LOL, TS):
-        return self.extractionAllIterations(rawData, LOL, TS)
+    def extractAllIterations(self, rawData, LOL, copy=True):
+        return self.extractionAllIterations(rawData, LOL, copy)
+
+    #_________________________
+
+    def removeFilter(self, data, LOL):
+        if LOL == 'lin':
+            mini = self.minValue
+        elif LOL == 'log':
+            mini = 0.0
+
+        return np.maximum(data, mini)
 
     #_________________________
 

analyse/utils/analyse/scaling/scaling.py
@@ -37,12 +37,17 @@ def arrayToScaling(array):
 
 #__________________________________________________
 
-def computeScaling(matrix):
+def computeScaling(matrix, minValue, defaultMaxValue):
     scaling      = Scaling()
     scaling.mean = matrix.mean()
     scaling.var  = matrix.var()
-    scaling.mini = matrix.min()
+
+    scaling.mini = minValue
     scaling.maxi = matrix.max()
+
+    if scaling.maxi <= minValue:
+        scaling.maxi = defaultMaxValue
+
     return scaling
 
 #__________________________________________________
@@ -80,7 +85,7 @@ def mergeScalings(simOutput, scalings, maximums, AOG):
             scale.meanMeans                 = meanMeans
             scale.geomMeanMeans             = geomMeanMeans
             scale.meanVars                  = meanVars
-            scale.sumMaximum                = maximums[lol][field.name].sum()
+            scale.sumMaximum                = np.maximum(maximums[lol][field.name], 0.0).sum()
             mergedScalings[field.name][lol] = scale
 
     return mergedScalings